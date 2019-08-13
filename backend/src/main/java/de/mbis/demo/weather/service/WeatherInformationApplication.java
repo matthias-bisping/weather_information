6549ceaab0cb549de88cb9733f77b9b757da0b25
matthias-bisping <matthias.bisping@gmx.de>
2019-08-13 11:53:33 +0200

    initial

---
+++ backend/src/main/java/de/mbis/demo/weather/service/WeatherInformationApplication.java
@@ -0,0 +1,13 @@
+package de.mbis.demo.weather.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherInformationApplication.class, args);
+    }
+
+}
