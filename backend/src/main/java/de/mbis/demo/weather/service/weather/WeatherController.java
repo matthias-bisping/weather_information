768658a3e2e33cfef386a91eb1b681c44c33c81e
matthias-bisping <matthias.bisping@gmx.de>
2019-08-23 10:48:12 +0200

    add backend logging

--- backend/src/main/java/de/mbis/demo/weather/service/weather/WeatherController.java
+++ backend/src/main/java/de/mbis/demo/weather/service/weather/WeatherController.java
@@ -2,6 +2,8 @@ package de.mbis.demo.weather.service.weather;
 
 import de.mbis.demo.weather.service.exceptions.CityNotFoundException;
 import de.mbis.demo.weather.service.provider.OpenWeatherProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("rest/")
 public class WeatherController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private OpenWeatherProvider provider;
 
     @Value("${provider.openweather.icon.url.template}")
@@ -23,9 +27,11 @@ public class WeatherController {
 
     @RequestMapping("/currentWeather")
     public CurrentWeatherDto currentWeather(@RequestParam("city") String city) {
+        logger.info("try to receive current weather for: {}", city);
         try {
             return CurrentWeatherDto.toDto(provider.getCurrentWeatherByCityName(city), iconUrlTemplate);
         } catch (CityNotFoundException e) {
+            logger.error("error on receiving current weather", e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
