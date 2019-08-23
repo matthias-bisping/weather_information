7ac738e5a5adbd3250d27a7b078393529014963e
matthias-bisping <matthias.bisping@gmx.de>
2019-08-23 10:36:14 +0200

    karma tests via maven, without watch, headless

--- frontend/src/app/manual/manual.component.spec.ts
+++ frontend/src/app/manual/manual.component.spec.ts
@@ -11,6 +11,7 @@ import {MockRestApiService} from "../shared/app.testdata";
 describe('ManualComponent', () => {
   let component: ManualComponent;
   let fixture: ComponentFixture<ManualComponent>;
+  let compiled: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,9 +28,28 @@ describe('ManualComponent', () => {
     fixture = TestBed.createComponent(ManualComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain update button', () => {
+    expect(compiled.querySelector('button').textContent).toContain('Update');
+  });
+
+  it('should have invalid form on empty input fields', () => {
+    expect(component.manualUpdate.valid).toBeFalsy();
+  });
+
+  it('should have valid form on filled input fields', () => {
+    component.manualUpdate.controls["city"].setValue('Oelde');
+    expect(component.manualUpdate.valid).toBeTruthy();
+  });
+
+  it('should set current weather on submit form', () => {
+    component.submitManualUpdate();
+    expect(component.currentWeather).not.toBeNull();
   });
 });
